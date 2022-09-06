eefa3d03f62f46d38247c89ab206c5f9fe57c88e
Usuario <Usuario@CPD03-M04>
2022-09-06 11:49:43 -0300

    Adicionar arquivos de projeto.

---
+++ AppCustoViagem/AppCustoViagem/Model/Viagem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppCustoViagem.Model
+{
+    public class Viagem
+    {
+        public int Id { get; set; }
+        public string Origem { get; set; }
+        public double Destino { get; set; }
+
+        public double Distancia { get; set; }
+
+        public double Consumo { get; set; }
+
+        public double Preco_Combustivel { get; set; }
+    }
+}
